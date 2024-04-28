dc9096b452de457d0f5739b5132e798e0df8788c
Mheyrie <omota4fidelity@gmail.com>
2024-04-28 23:52:35 +0100

    tailwind setup completed amd Navbar component initiated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="text-yellow-500">
+        jsjshvklvbszh
+        <h1 className="text-3xl font-bold underline">Whay arr tom jfv c</h1>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'Fraunces', 'Roboto'],
+        sans:['Roboto', 'sans-serif']
+    },
+    colors:{
+      brightBlu: '#3EBEFF',
+    }, 
+    screens:{
+      sm: '480px',
+      md: '850px',
+      lg: '1440px'
+    }
+    },
+  },
+  plugins: [],
+}
