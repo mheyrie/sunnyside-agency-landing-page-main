fd561c643fc9b6436c9df0a33a7df0367faa3df2
Mheyrie <omota4fidelity@gmail.com>
2024-04-28 20:35:59 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/sunnyside-agency-landing-page-main/',
   server: {
     port: 3000,
   }
