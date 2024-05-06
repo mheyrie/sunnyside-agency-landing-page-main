82f15f550e1ed1fe80b4e604a30aad1bf71c81f5
Mheyrie <omota4fidelity@gmail.com>
2024-05-06 05:01:51 +0100

    Aligmnent of Herosection bg-image ongoing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const HeroSection = () => {
     
   return (
-    <div className="bg-heroSection bg-auto bg-no-repeat bg-center h-96">
-        <h1 className="text-center text-white"> WE ARE CREATIVES</h1>
+    <div className="bg-heroSection bg-contain bg-no-repeat  h-96 w-auto flex justify-center items-center mt-[-10px]">
+      <h1 className="text-center  text-white"> WE ARE CREATIVES</h1>
     </div>
+    
+    
   )
 }
 
