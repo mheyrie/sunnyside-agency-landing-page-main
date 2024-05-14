3adc73720c6dd0b7cd3475a04a618f58e8d258ad
Mheyrie <omota4fidelity@gmail.com>
2024-05-14 23:33:44 +0100

    Updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const HeroSection = () => {
     
   return (
-    <div className="bg-heroSection bg-contain bg-no-repeat  h-96 w-auto flex justify-center items-center mt-[-10px]">
+    <div className="bg-heroSection bg-cover bg-no-repeat h-96 flex justify-center items-center md:bg-contain mt-[-20px]">
       <h1 className="text-center  text-white"> WE ARE CREATIVES</h1>
     </div>
     
-    
+
+     
   )
 }
 
